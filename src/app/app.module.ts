0cdc354cd7620d99e0f89145de811cadf250e13d
Bertrand Jaunet <bertrand.jaunet@gmail.com>
2017-07-26 20:54:57 +0200

    creation du service henri potier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HenriPotierService } from './henripotier.service';
+
+// import des styles Material Design
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdToolbarModule, MdInputModule, MdCardModule, MdGridListModule, MdCheckboxModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HenriPotierService
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    [BrowserAnimationsModule],
+    [MdButtonModule, MdToolbarModule, MdInputModule, MdCardModule, MdGridListModule, MdCheckboxModule],
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/henripotier.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
+@Injectable()
+export class HenriPotierService {
+
+  constructor(private _http: Http) { }
+  // méthode faisant appel a l'api de http://henri-potier.xebia.fr/ (GET)
+
+  getBooks() {
+    const url = 'http://henri-potier.xebia.fr/books';
+    console.log('url getBooks: ' + url);
+    return this._http.get(url)
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw('Server error'));
+  }
+
+  getDiscount(lesIsbn) {
+    const url = 'http://henri-potier.xebia.fr/books';
+    for (var i = 0; i <= lesIsbn.length; i++) {
+      url.concat(lesIsbn[i]);
+      if (i < lesIsbn.length){
+        url.concat(',');
+      } else {
+        url.concat('/commercialOffers');
+      }
+    }
+    console.log('discountUrl: ' + url);
+    return this._http.get(url)
+      .map((res: Response) => res.json())
+      .catch((error:any) => Observable.throw('Server error'));
+  }
+
+}
